bf6d03288daebbd4b4222b37017af34cb4bca44f
OliverEhrhardt <oliver.ehrhardt0@gmail.com>
2015-05-12 21:41:59 -0600

    added random button

---
+++ Project/Assets/Scripts/RandomButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomButton : MonoBehaviour {
+
+	//////////////PUBLIC VARIABLES//////////////
+	
+	//scroll bars controll rgb values depending on which scroll bar
+	public GameObject redScrollBar;
+	public GameObject blueScrollBar;
+	public GameObject greenScrollBar;
+
+	//////////////PRIVATE VARIABLES//////////////
+
+	//get slider component
+	private UISlider slider;
+
+	void OnClick(){
+		//randomize red
+		slider = redScrollBar.GetComponent<UISlider> ();
+		slider.value = (Random.Range(0,11) * 0.1f);
+		//randomize green
+		slider = greenScrollBar.GetComponent<UISlider> ();
+		slider.value = (Random.Range(0,11) * 0.1f);
+		//randomize blue
+		slider = blueScrollBar.GetComponent<UISlider> ();
+		slider.value = (Random.Range(0,11) * 0.1f);
+	}
+}
